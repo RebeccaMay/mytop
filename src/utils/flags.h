900c5ea000b32a1ae318401d72ffed4f46875f01
Rebecca May <remay@bb136-01.adit.mines.edu>
2017-02-23 10:21:16 -0700

    flag files added

---
+++ src/utils/flags.h
@@ -0,0 +1,18 @@
+#pragma once
+
+enum sortby {PID, CPU, MEM, TIME};
+
+struct progopts{
+  int delay_tenths;
+  enum sortby sort_key;
+  int max_proc;
+};
+
+extern struct progopts opts;
+
+static const char sortby_keys[4][5] = {"PID", "CPU", "MEM", "TIME"};
+
+void opts_help(char **argv, int exit_status);
+
+void opts_init(int argc, char **argv);
+  
